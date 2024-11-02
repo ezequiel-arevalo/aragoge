a6b36f8a7806d18b9c61a30707cbe43c885d9d78
Ezequiel Arevalo <ezequiel.thomas.arevalo@gmail.com>
2024-11-02 01:41:55 -0300

    hotfix: img url

--- src/pages/auth/register/components/ImageSection.jsx
+++ src/pages/auth/register/components/ImageSection.jsx
@@ -13,7 +13,7 @@ export const ImageSection = () => {
       overflow="hidden"
     >
       <Image
-        src="./src/pages/auth/login/components/aragoge.svg"
+        src="./src/pages/auth/register/components/aragoge.svg"
         alt="Login illustration"
         objectFit="cover"
         height="full"
